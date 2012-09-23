077cdd50f29177c4df3c396b0c9059603aaad4db
Raghu Betina <rbetina@gmail.com>
2012-09-23 14:58:18 -0500

    Slide image working.

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,5 +1,5 @@
 class Slide < ActiveRecord::Base
-  attr_accessible :position, :slide_image, :slide_audio, :user_id
+  attr_accessible :position, :slide_image, :slide_audio, :user_id, :remove_slide_image
   
   mount_uploader :slide_image, SlideImageUploader
   mount_uploader :slide_audio, SlideAudioUploader
